508e33137a8294bb4b5c3be351178af0ef5d864f
Yuki Ichinose <yichinos@c1r16s4.42tokyo.jp>
2023-08-24 15:21:37 +0900

    ++

--- srcs/requiements/mariadb/setup.sh
+++ srcs/requiements/mariadb/setup.sh
@@ -1,37 +1,16 @@
 #!/bin/sh
 
-if [ ! -d "/run/mysql" ]; then
-	mkdir -p /run/mysqld
-fi
-chown -R mysql:mysql /run/mysqld
-
 if [ ! -d /var/lib/mysql/mysql ]; then
-	chown -R mysql:mysql /var/lib/mysql
-	chown -R mysql /var/lib/mysql
-	mysql_install_db --user=mysql --skip-test-db --datadir=/var/lib/mysql -basedir=/usr
-
-tfile=""
-  while [ ! -f "$tfile" ]; do
-    tfile=$(mktemp)
-  done
-	cat << EOF > $tfile
-USE mysql ;
+mysql_install_db --user=mysql --skip-test-db --datadir=/var/lib/mysql -basedir=/usr
+mysqld --user=mysql --bootstrap << EOF
 FLUSH PRIVILEGES ;
-DROP DATABASE IF EXISTS test ;
-GRANT ALL ON *.* TO 'root'@'%' identified by '$MYSQL_ROOT_PASSWORD' WITH GRANT OPTION ;
-GRANT ALL ON *.* TO 'root'@'localhost' identified by '$MYSQL_ROOT_PASSWORD' WITH GRANT OPTION ;
-SET PASSWORD FOR 'root'@'localhost'=PASSWORD('${MYSQL_ROOT_PASSWORD}') ;
-SET PASSWORD FOR 'root'@'%'=PASSWORD('${MYSQL_ROOT_PASSWORD}') ;
-FLUSH PRIVILEGES ;
-CREATE DATABASE IF NOT EXISTS $WP_DB_NAME CHARACTER SET utf8 COLLATE utf8_general_ci ;
-CREATE USER '$WP_DB_USER'@'localhost' IDENTIFIED BY '$WP_DB_PASSWORD';
 CREATE USER '$WP_DB_USER'@'%' IDENTIFIED BY '$WP_DB_PASSWORD';
-GRANT ALL PRIVILEGES ON *.* TO '$WP_DB_USER'@'localhost';
-GRANT ALL PRIVILEGES ON *.* TO '$WP_DB_USER'@'%';
+CREATE DATABASE $WP_DB_NAME ;
+GRANT ALL ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';
+DELETE FROM mysql.user WHERE User='';
+DELETE FROM mysql.user WHERE User='root'
 FLUSH PRIVILEGES ;
 EOF
-/usr/bin/mysqld --user=mysql --bootstrap < $tfile
-  rm -f $tfile
 fi
 
-exec /usr/bin/mysqld --user=mysql --console
+exec mysqld --user=mysql --console
