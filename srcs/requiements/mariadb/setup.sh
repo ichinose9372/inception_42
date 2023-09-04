11a0cc05f8f6fc41069bbce2a8488d48289e7e26
yichinos <ichinose.0720.emt@gmail.com>
2023-09-05 08:49:25 +0900

    +

--- srcs/requiements/mariadb/setup.sh
+++ srcs/requiements/mariadb/setup.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
 if [ ! -d /var/lib/mysql/mysql ]; then
+chown -R mysql:mysql /var/lib/mysql
+chown -R mysql /var/lib/mysql	
 mysql_install_db --user=mysql --skip-test-db --datadir=/var/lib/mysql -basedir=/usr
 mysqld --user=mysql --bootstrap << EOF
 USE mysql ;
