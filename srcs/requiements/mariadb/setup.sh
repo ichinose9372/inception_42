a20fb71c4f8eea29c98795bb9b60f7d31cf7399d
yichinos <ichinose.0720.emt@gmail.com>
2023-09-04 23:28:15 +0900

    +++

--- srcs/requiements/mariadb/setup.sh
+++ srcs/requiements/mariadb/setup.sh
@@ -3,13 +3,19 @@
 if [ ! -d /var/lib/mysql/mysql ]; then
 mysql_install_db --user=mysql --skip-test-db --datadir=/var/lib/mysql -basedir=/usr
 mysqld --user=mysql --bootstrap << EOF
+USE mysql ;
 FLUSH PRIVILEGES ;
+DROP DATABASE IF EXISTS test ;
+GRANT ALL ON *.* TO 'root'@'%' identified by '$MYSQL_ROOT_PASSWORD' WITH GRANT OPTION ;
+GRANT ALL ON *.* TO 'root'@'localhost' identified by '$MYSQL_ROOT_PASSWORD' WITH GRANT OPTION ;
+SET PASSWORD FOR 'root'@'localhost'=PASSWORD('${MYSQL_ROOT_PASSWORD}') ;
+SET PASSWORD FOR 'root'@'%'=PASSWORD('${MYSQL_ROOT_PASSWORD}') ;
+FLUSH PRIVILEGES ;
+CREATE DATABASE IF NOT EXISTS $WP_DB_NAME CHARACTER SET utf8 COLLATE utf8_general_ci ;
+CREATE USER '$WP_DB_USER'@'localhost' IDENTIFIED BY '$WP_DB_PASSWORD';
 CREATE USER '$WP_DB_USER'@'%' IDENTIFIED BY '$WP_DB_PASSWORD';
-CREATE DATABASE $WP_DB_NAME ;
-USE $WP_DB_NAME ;
-GRANT ALL PRIVILEGES ON $WP_DB_NAME.* TO '$WP_DB_USER'@'%';
-DELETE FROM mysql.user WHERE User='';
-DELETE FROM mysql.user WHERE User='root';
+GRANT ALL PRIVILEGES ON *.* TO '$WP_DB_USER'@'localhost';
+GRANT ALL PRIVILEGES ON *.* TO '$WP_DB_USER'@'%';
 FLUSH PRIVILEGES ;
 EOF
 fi
