0ab5feae11794fa65de1096807f336053c93da48
yichinos <ichinose.0720.emt@gmail.com>
2023-09-07 15:34:21 +0900

    +

--- srcs/requiements/mariadb/setup.sh
+++ srcs/requiements/mariadb/setup.sh
@@ -1,25 +1,37 @@
-#!/bin/sh
-
 if [ ! -d "/run/mysqld" ]; then
-
-	mkdir -p /run/mysqld
-	chown -R mysql:mysql /run/mysqld
+    mkdir -p /run/mysqld
+    chown -R mysql:mysql /run/mysqld
 fi
 
 if [ ! -d /var/lib/mysql/mysql ]; then
     mysql_install_db --user=mysql --skip-test-db --basedir=/usr --datadir=/var/lib/mysql
 
-    mysqld -u mysql --bootstrap <<EOF
-		flush privileges;
-		create user '${WP_DB_USER}'@'%' identified by '${WP_DB_PASSWORD}';
-		create database ${WP_DB_NAME};
-		grant all on ${WP_DB_NAME}.* to '${WP_DB_USER}'@'%';
-		delete from mysql.user where user='';
-		delete from mysql.user where user='root';
-		flush privileges;
-EOF
+    mysqld --user=mysql --skip-networking & # バックグラウンドでmysqldを起動
 
+    # mysqldが起動するまで待機（最大60秒）
+    for i in $(seq 1 60); do
+        if mysqladmin ping &>/dev/null; then
+            break
+        fi
+        sleep 1
+    done
+
+    if [ "$i" -eq 60 ]; then
+        echo "MySQL server took too long to start. Aborting."
+        exit 1
+    fi
+
+    # ルートユーザのパスワードを設定
+    mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';"
+
+    # データベースとユーザーの作成
+    mysql -e "CREATE DATABASE IF NOT EXISTS $WP_DB_NAME CHARACTER SET utf8 COLLATE utf8_general_ci;"
+    mysql -e "CREATE USER '$WP_DB_USER'@'%' IDENTIFIED BY '$WP_DB_PASSWORD';"
+    mysql -e "GRANT ALL PRIVILEGES ON $WP_DB_NAME.* TO '$WP_DB_USER'@'%';"
+    mysql -e "FLUSH PRIVILEGES;"
+
+    # mysqldを終了してフォアグラウンドで再起動
+    mysqladmin shutdown
 fi
-
 
 exec mysqld --user=mysql --console
