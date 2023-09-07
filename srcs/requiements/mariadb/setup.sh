4100625f9f9ced1723367ccd2cc50a5eb6a64ab6
yichinos <ichinose.0720.emt@gmail.com>
2023-09-07 14:17:35 +0900

    +

--- srcs/requiements/mariadb/setup.sh
+++ srcs/requiements/mariadb/setup.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
-sudo chown -R _mysql /usr/local/var/mysql
-sudo chmod -R o+rwx /usr/local/var/mysql
-
 if [ ! -d "/run/mysqld" ]; then
 
 	mkdir -p /run/mysqld
@@ -24,45 +21,5 @@ EOF
 
 fi
 
-# # chown -R mysql:mysql /var/lib/mysql
-# # chown -R mysql /var/lib/mysql	
-# # mysql_install_db --user=mysql --skip-test-db --datadir=/var/lib/mysql -basedir=/usr
-# # mysqld --user=mysql --bootstrap << EOF
-# # USE mysql ;
-# # FLUSH PRIVILEGES ;
-# # DROP DATABASE IF EXISTS test ;
-# # GRANT ALL ON *.* TO 'root'@'%' identified by '$MYSQL_ROOT_PASSWORD' WITH GRANT OPTION ;
-# # GRANT ALL ON *.* TO 'root'@'localhost' identified by '$MYSQL_ROOT_PASSWORD' WITH GRANT OPTION ;
-# # SET PASSWORD FOR 'root'@'localhost'=PASSWORD('${MYSQL_ROOT_PASSWORD}') ;
-# # SET PASSWORD FOR 'root'@'%'=PASSWORD('${MYSQL_ROOT_PASSWORD}') ;
-# # FLUSH PRIVILEGES ;
-# # CREATE DATABASE IF NOT EXISTS $WP_DB_NAME CHARACTER SET utf8 COLLATE utf8_general_ci ;
-# # CREATE USER '$WP_DB_USER'@'localhost' IDENTIFIED BY '$WP_DB_PASSWORD';
-# # CREATE USER '$WP_DB_USER'@'%' IDENTIFIED BY '$WP_DB_PASSWORD';
-# # GRANT ALL PRIVILEGES ON *.* TO '$WP_DB_USER'@'localhost';
-# # GRANT ALL PRIVILEGES ON *.* TO '$WP_DB_USER'@'%';
-# # FLUSH PRIVILEGES ;
-# # EOF
-# # fi
 
-# exec mysqld --user=mysql --console
-
-
-#!/bin/sh
-
-# MariaDBのデータディレクトリの初期化
-# mysql_install_db --user=mysql --skip-test-db --datadir=/var/lib/mysql --basedir=/usr
-# chown -R mysql:mysql /var/lib/mysql
-# # MariaDBを起動して設定を適用
-# mysqld --user=mysql --bootstrap << EOF
-# USE mysql;
-# FLUSH PRIVILEGES;
-# ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';
-# CREATE DATABASE IF NOT EXISTS $WP_DB_NAME CHARACTER SET utf8 COLLATE utf8_general_ci;
-# CREATE USER '$WP_DB_USER'@'localhost' IDENTIFIED BY '$WP_DB_PASSWORD';
-# GRANT ALL PRIVILEGES ON $WP_DB_NAME.* TO '$WP_DB_USER'@'localhost';
-# FLUSH PRIVILEGES;
-# EOF
-
-# MariaDBをフォアグラウンドで起動
 exec mysqld --user=mysql --console
