c9ac5835d476251ea41efb99300bfb153b517235
yichinos <ichinose.0720.emt@gmail.com>
2023-09-23 17:48:41 +0900

    push

--- srcs/requiements/mariadb/setup.sh
+++ srcs/requiements/mariadb/setup.sh
@@ -4,7 +4,7 @@
 # if [ ! -d /var/lib/mysql/mysql ]; then
 #     mysql_install_db --user=mysql --skip-test-db --basedir=/usr --datadir=/var/lib/mysql
 
-#     mysqld -u mysql --bootstrap <<EOF
+# 	mysqld -u mysql --bootstrap <<EOF
 # 		flush privileges;
 # 		create user '${WP_DB_USER}'@'%' identified by '${WP_DB_PASSWORD}';
 # 		create database ${WP_DB_NAME};
@@ -16,7 +16,7 @@
 
 # fi
 
-# exec mysqld --user=mysql --console
+exec mysqld --user=mysql --console
 
 
 echo "CREATE DATABASE IF NOT EXISTS ${WP_DB_NAME};" > db1.sql
