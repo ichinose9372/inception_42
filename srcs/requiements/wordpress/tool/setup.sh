241d00f59cd6d519cb4cd3dfcf923d57b65ef22e
yichinos <ichinose.0720.emt@gmail.com>
2023-10-04 12:24:17 +0900

    push

--- srcs/requiements/wordpress/tool/setup.sh
+++ srcs/requiements/wordpress/tool/setup.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
-# while ! mariadb -h $MYSQL_HOST -u $WP_DB_USER -p$WP_DB_PASSWORD $WP_DB_NAME --silent 2>/dev/null; do
-# 	sleep 1 
-# done 
 
 #wordpress
 wp core download --allow-root
@@ -16,8 +13,7 @@ wp core install \
 	--title="yichinos is" \
 	--admin_user=$WP_USER \
 	--admin_password=$WP_PASSWORD \
-	--admin_email=$WP_ADMIN_EMAIL \
-	--url=$DOMAIN_NAME 
+	--admin_email=$WP_ADMIN_EMAIL 
 
 sed -i "/That's all, stop editing!/a define('RELOCATE', true);" wp-config.php
 
